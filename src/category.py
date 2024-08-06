c8a7e83b26e973300839c8f2084b262a9ac9ce42
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-08-06 19:53:58 +0300

    add methods products and add_product in class Products

--- src/category.py
+++ src/category.py
@@ -1,10 +1,13 @@
+from src.product import Product
+
+
 class Category:
     """
     Класс категорий продуктов интернет магазина
     """
     name: str
     description: str
-    products: list
+    __products: list
     category_count: int = 0
     product_count: int = 0
 
@@ -14,6 +17,24 @@ class Category:
         """
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def products(self) -> str:
+        """
+        Геттер атрибута __products
+        """
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
+
+    def add_product(self, new_product: Product) -> None:
+        """
+        Метод добавляет новый продукт в список продуктов
+        """
+        self.__products.append(new_product)
+        Category.product_count += 1
+
